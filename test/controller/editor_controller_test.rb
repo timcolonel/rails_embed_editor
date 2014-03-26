139cd8344995c4bd36d743c9ed12c4dfc59dd9c7
Timothee Guerin <timothee.guerin@outlook.com>
2014-03-26 14:16:10 -0400

    Added unit test when working

--- test/controller/editor_controller_test.rb
+++ test/controller/editor_controller_test.rb
@@ -3,15 +3,31 @@ require 'test_helper'
 class EditorControllerTest < ActionController::TestCase
   tests RailsEmbedEditor::EditorController
 
+  def destination
+    File.expand_path('../../tmp/edit_test', __FILE__)
+  end
+
+  def filename
+    "#{destination}/dumbfile.erb"
+  end
+
+  def setup
+    FileUtils.mkdir_p(File.dirname(filename))
+    FileUtils.cp 'test/rails_embed_editor/dumbfile.erb', filename
+  end
+
+
   test 'should not edit' do
     get :edit
     assert (not JSON.parse(@response.body)['success']), "Should return not success but returned #{@response.body}"
   end
 
   test 'should edit file' do
-
-    #get :edit
-    #assert(JSON.parse @response.body['success'], "Should return success but returned #{@response.body}")
+    first_line = 2
+    last_line = 5
+    text= 'Replace with some othe ansowmn content'
+    get :edit, :filename => filename, :first_line => first_line, :last_line => last_line, :content => text
+    assert(JSON.parse(@response.body)['success'], "Should return success but returned #{@response.body}")
   end
 
 
